a2716acaf79873f037eda733aaa4520372f188ae
sriramulasaichander <sriramulasaichander@gmail.com>
2021-08-01 10:56:21 +0000

    changes done for windows checking

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
 	 
     System.out.println("TRYING WEBHOOKS ");
-    System.out.println("changing for checking webhook");
+    System.out.println("changing for checking webhooks");
   }
 }
