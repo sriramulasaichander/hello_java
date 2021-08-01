dcd1b72f848d048b8b0e1c947db1564bf7bcb903
sriramulasaichander <sriramulasaichander@gmail.com>
2021-08-01 10:08:18 +0000

    done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
 	 
-    System.out.println("CODE MODIFIED FOR CHECKING POLL SCM OPTION");
+    System.out.println("CODE MODIFIED ");
 
   }
 }
--- sampleShell.sh
+++
@@ -1,1 +0,0 @@
-echo this is first shell script file pushing to git and created a job in jenkins
