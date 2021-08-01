e4df2a7666726704dc00ec70e69a62dec21939c3
sriramulasaichander <sriramulasaichander@gmail.com>
2021-08-01 06:45:35 +0000

    changes done in java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("WELCOME TO JAVA WORLD WITH JENKINSCHANGING THE CONTENT TO TEST JENKINS ");
+    System.out.println("WELCOME TO JAVA WORLD WITH JENKINSCHANGING THE CONTENT TO TEST JENKINS CHANGING THE CONTENET");
   }
 }
